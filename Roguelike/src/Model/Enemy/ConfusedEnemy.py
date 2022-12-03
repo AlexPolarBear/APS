94089a612f98f02e602a8958febc70f21e473f0d
Vladislav Ushakov <vladislav.ushakov@soteranalytics.com>
2022-12-03 20:11:26 +0300

    Removed redundant lines

--- Roguelike/src/Model/Enemy/ConfusedEnemy.py
+++ Roguelike/src/Model/Enemy/ConfusedEnemy.py
@@ -23,14 +23,6 @@ class ConfusedEnemy(Enemy):
     @health.setter
     def health(self, value: int):
         self._enemy.health = value
-
-    @property
-    def status(self) -> CharacterStatus:
-        return self._enemy.status
-
-    @property
-    def attack(self) -> int:
-        return self._enemy.attack
     
     def next_move(self, current_position: (int, int), map) -> (int, int):
         return self._enemy.next_move(current_position, map)
