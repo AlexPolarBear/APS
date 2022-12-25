5f5726690cd8afdb59c821662a1416512dde8084
Saveliy Grigoryev <sava-cska@mail.ru>
2022-12-26 00:32:02 +0300

    Fix defence

--- Roguelike/src/Model/Enemy/EnemyContext.py
+++ Roguelike/src/Model/Enemy/EnemyContext.py
@@ -57,3 +57,14 @@ class EnemyContext(object):
 
     def clone(self):
         return self._enemy.clone()
+    
+    def defence(self, attack: int):
+        if self.health >= CRITICAL_LEVEL and self.health - value < CRITICAL_LEVEL:
+            self._original_state = self._enemy
+            self._enemy = CowardEnemy(self._enemy.health, self._enemy.attack, self._enemy.style)
+        elif self.health < CRITICAL_LEVEL and self.Health - value >= CRITICAL_LEVEL:
+            self._enemy = self._original_state
+
+        self._enemy.defence(attack)
+        if self._enemy is not self._original_state:
+            self._original_state.defence(attack)
