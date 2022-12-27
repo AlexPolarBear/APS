e58fd8ba440882b35bbbb2108bf9a6c2a17f64b7
Vladislav Ushakov <vladislav.ushakov@soteranalytics.com>
2022-12-27 19:51:07 +0300

    Updated docs

--- Roguelike/test/Model/test_Item.py
+++ Roguelike/test/Model/test_Item.py
@@ -1,16 +1,17 @@
 from src.Model.Item import Item
 
+
 def test_Item1():
     item = Item(10)
-    
+
     assert item._health_point == 10
-    assert item._activated == False
+    assert not item._activated
 
     assert item.health_point == 10
-    assert item.activated == False
+    assert not item.activated
 
     item.activate()
-    assert item.activated == True
+    assert item.activated
 
     item.deactivate()
-    assert item.activated == False
+    assert not item.activated
